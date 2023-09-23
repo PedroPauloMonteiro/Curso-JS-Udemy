660642228ba98e65d77d35910951656ab7cc6764
PedroPauloMonteiro <paulopedromr2552@gmail.com>
2023-09-23 10:00:15 -0300

    Aula 41

---
+++ Aula 41/index.js
@@ -0,0 +1,8 @@
+/*function biggerN(x, y) {
+    return x>y ? `O maior numero é o ${x}`: `O maior numero é o ${y}`
+}
+
+console.log(biggerN(7,5)) */
+
+const max2 = (x, y) => x>y ? x : y;
+console.log(max2(7,5))
