7d9e24f0a010c7662cf189b56e1314dd71032602
PedroPauloMonteiro <paulopedromr2552@gmail.com>
2023-09-23 07:58:33 -0300

    Aula 36

---
+++ Aula 36/index.js
@@ -0,0 +1,16 @@
+/*const frutas = ['Pera', 'Maçã', 'Uva'];
+
+for (let i in frutas) {
+    console.log(frutas[i])
+} 
+*/
+
+const pessoa = {
+    nome: 'Pedro Paulo',
+    sobrenome: 'Rocha',
+    idade: 18
+};
+
+for (let chave in pessoa) {
+    console.log(chave, '=', pessoa[chave])
+}
