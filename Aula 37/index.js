fb4dbda17feb9e4d0131c0702ce8a6162b572fea
PedroPauloMonteiro <paulopedromr2552@gmail.com>
2023-09-23 08:31:25 -0300

    Aula 37

---
+++ Aula 37/index.js
@@ -0,0 +1,18 @@
+/*const nome = 'Pedro Paulo';
+
+for (valor of nome) {
+    console.log(valor)
+}
+*/
+/*nomes = ['Pedro', 'Paulo', 'Luiz'];
+
+nomes.forEach(function(el) {
+    console.log(el)
+});
+*/
+
+const nome = 'Pedro Paulo'
+
+for (let valor of nome) {
+    console.log(valor)
+}
