35c942d85d98b781ab34c9331e509e02652482df
PedroPauloMonteiro <paulopedromr2552@gmail.com>
2023-10-18 09:16:14 -0300

    Aula 62 e 63

---
+++ Aula 62/index.js
@@ -0,0 +1,11 @@
+/*const nomes = ['Maria', 'Joana', 'Marcos', 'Wallace', 'Rosana'];
+//const novo = [...nomes];
+const novo = nomes.slice(0, -2)
+console.log(novo);*/
+
+//const nome = 'Pedro Paulo Rocha';
+//const nomes = nome.split(' ');
+
+const nomes = ['Pedro', 'Paulo', 'Rocha'];
+const nome = nomes.join(' ');
+console.log(nome);
---
+++ Aula 63/index.js
@@ -0,0 +1,4 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+const removidos = nomes.splice(nomes.length, 0, 'Luiz', 'Marcio', 'Souza');
+console.log(nomes);
+console.log(removidos);
